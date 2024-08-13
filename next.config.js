1b6099ac991e9411e5ddc8083d1bc4d79900e8b2
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2024-08-13 08:21:24 -0300

    save new errors

--- next.config.js
+++ next.config.js
@@ -7,5 +7,14 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/v0/b/**',
+      },
+    ],
+  },
 };
 module.exports = withPWA(nextConfig);
